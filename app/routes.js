5853b69c2ecea546d22cf630910409c9f340dc38
eliza <lisakonovalova@gmail.com>
2016-09-14 04:19:22 +1000

    - Finally connected to todoist

---
+++ app/routes.js
@@ -0,0 +1,112 @@
+module.exports = function(app, passport) {
+
+	app.get('/', function(req, res) {
+		res.render('index.ejs');
+	});
+
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user
+		});
+	});
+
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	//Local Login
+	app.get('/login', function(req, res){res.render('login.ejs', { message: req.flash('loginMessage') });});
+	app.post('/login',passport.authenticate('local-login',{successRedirect:'/profile',failureRedirect:'/login',
+		failureFlash : true}));
+
+	//Local Sign up
+	app.get('/signup',function(req, res){res.render('signup.ejs',{message:req.flash('loginMessage')});});
+	app.post('/signup', passport.authenticate('local-signup',{successRedirect:'/profile',failureRedirect:'/signup',
+		failureFlash : true}));
+
+	//Facebook Authentication
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['public_profile,email,user_actions.news,user_actions.video,user_events,user_likes,' +
+		'user_photos,user_posts,manage_pages,publish_pages,rsvp_event,user_friends'] }));
+	app.get('/auth/facebook/callback',passport.authenticate('facebook',{successRedirect:'/profile',failureRedirect:'/'}));
+
+	//Twitter Authentication
+	app.get('/auth/twitter', passport.authenticate('twitter', {scope:'email'}));
+	app.get('/auth/twitter/callback',passport.authenticate('twitter',{successRedirect:'/profile',failureRedirect:'/'}));
+
+	//Todoist Authentication
+	app.get('/auth/todoist',passport.authenticate('todoist',{scope:['data:read_write', 'data:delete'],state: 'secretstring'}));
+	app.get('/auth/todoist/callback', passport.authenticate('todoist', {successRedirect:'/profile', failureRedirect:'/'}));
+
+	//Google Authentication
+	app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+	app.get('/auth/google/callback', passport.authenticate('google', {successRedirect : '/profile',failureRedirect : '/'}));
+
+
+	//Connect Local
+	app.get('/connect/local', function(req, res){res.render('connect-local.ejs',{message: req.flash('loginMessage')});});
+	app.post('/connect/local',passport.authenticate('local-signup',{successRedirect:'/profile',failureRedirect : '/connect/local',
+			failureFlash : true}));
+
+	//Connect Facebook
+	app.get('/connect/facebook', passport.authorize('facebook', {scope : ['public_profile,email,user_actions.news,' +
+	'user_actions.video,user_events,user_likes,user_photos,user_posts,manage_pages,publish_pages,rsvp_event,user_friends']}));
+	app.get('/connect/facebook/callback',passport.authorize('facebook',{successRedirect:'/profile',failureRedirect : '/'}));
+
+	//Connect Twitter
+	app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+	app.get('/connect/twitter/callback',passport.authorize('twitter',{successRedirect : '/profile',
+				failureRedirect : '/'}));
+
+	//Connect Google
+	app.get('/connect/google', passport.authorize('google', { scope : ['profile', 'email'] }));
+	app.get('/connect/google/callback',passport.authorize('google',{successRedirect : '/profile',
+				failureRedirect : '/'}));
+
+	//Connect Todoist
+	app.get('/connect/todoist', passport.authorize('todoist',{scope:['data:read_write', 'data:delete'],state: 'secretstring'}));
+	app.get('/connect/todoist/callback',passport.authorize('todoist',{successRedirect : '/profile',
+		failureRedirect : '/'}));
+
+	app.get('/unlink/local', function(req, res) {
+		var user = req.user;
+		user.local.email = undefined;
+		user.local.password = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+	app.get('/unlink/facebook', function(req, res) {
+		var user            = req.user;
+		user.facebook.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+	app.get('/unlink/twitter', function(req, res) {
+		var user           = req.user;
+		user.twitter.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+	app.get('/unlink/google', function(req, res) {
+		var user = req.user;
+		user.google.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+
+};
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+
+	res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.configure(function() {
+
+	// set up our express application
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); // read cookies (needed for auth)
+	app.use(express.bodyParser()); // get information from html forms
+
+	app.set('view engine', 'ejs'); // set up ejs for templating
+
+	app.use(express.session({ secret: 'secretsession' }));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(flash());
+
+});
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
